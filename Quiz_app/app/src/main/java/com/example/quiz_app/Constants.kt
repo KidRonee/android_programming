8fff3e3f3f17d094d00f41c611a75ca7b6f7cc8d
Mathi <mathias-b@hotmail.no>
2022-01-29 16:42:32 +0100

    refactor of xml files. Fixed launch bug by cleaning project.

---
+++ Quiz_app/app/src/main/java/com/example/quiz_app/Constants.kt
@@ -0,0 +1,60 @@
+package com.example.quiz_app
+
+import java.util.ArrayList
+
+object Constants {
+
+    fun getQuestions():ArrayList<Question> {
+        val questionsList = ArrayList<Question>()
+
+
+        val questionOne = Question(
+            1, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina", "Norway", "Russia", "Demacian",
+            1
+        )
+
+        val questionTwo = Question(
+            2, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Sweden", "Finland", "Norway", "Belgium",
+            4
+        )
+
+        val questionThree = Question(
+            3, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_fiji,
+            "Jordan", "Afghanistan", "Fiji", "France",
+            3
+        )
+
+        val questionFour = Question(
+            4, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "China", "India", "Vietnam", "Japan",
+            2
+        )
+
+        val questionFive = Question(
+            5, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Norway", "Sweden", "Germany", "Denmark",
+            4
+        )
+
+        val questionSix = Question(
+            1, "Which country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Brazil", "Peru", "Egypt", "Mexico",
+            1
+        )
+        questionsList.add(questionOne)
+        questionsList.add(questionTwo)
+        questionsList.add(questionThree)
+        questionsList.add(questionFour)
+        questionsList.add(questionFive)
+        questionsList.add(questionSix)
+        return questionsList
+    }
+}
---
+++ Quiz_app/app/src/main/java/com/example/quiz_app/QuizQuestionsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.quiz_app
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        val questionList = Constants.getQuestions()
+        Log.i("QuestionsList size is", " ${questionList.size}")
+
+    }
+}
